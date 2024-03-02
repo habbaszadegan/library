dcf13383d57d00221a0d40d9304012609b4951d2
Hossein Abbaszadegan <haabaszadegan@gmail.com>
2024-03-02 18:19:25 +0330

    Added modal dialog form input. Next: delete book.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const addBookBtn = document.querySelector('.add-book');
 const table = document.querySelector('table');
+const dialog = document.querySelector('dialog');
+const openModal = document.querySelector('.open-modal');
+const submit = document.querySelector('.submit');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const read = document.querySelector('#read');
 
 const library = [];
 
@@ -13,11 +20,11 @@ function Book(title, author, pages, read) {
     // }
 }
 
-function addBook() {
-    const addTitle = prompt('What is the title of the book?');
-    const addAuthor = prompt('Who is the author of the book?');
-    const addPages = prompt('How many pages does the book have?');
-    const readStatus = prompt('Have you read the book?');
+function addBookModal() {
+    const addTitle = title.value;
+    const addAuthor = author.value;
+    const addPages = pages.value;
+    const readStatus = read.value;
 
     return library.push(new Book(addTitle, addAuthor, addPages, readStatus));
 }
@@ -25,6 +32,7 @@ function addBook() {
 function displayBook(lib) {
     const bookTitles = [];
     const rows = table.querySelectorAll('tr');
+    let newRow = '';
 
     for (const row of rows) {
         // Assuming the title is stored in the first cell (td) of each row
@@ -36,7 +44,7 @@ function displayBook(lib) {
         if (bookTitles.includes(book.title)) {
             continue;
         }
-        const newRow = document.createElement('tr');
+         newRow = document.createElement('tr');
         for (const property in book) {
             const newCell = document.createElement('td');
             newCell.textContent = book[property];
@@ -44,11 +52,42 @@ function displayBook(lib) {
         }
         table.appendChild(newRow);
     }
+    const deleteBook = document.createElement('button');
+    deleteBook.textContent = 'Delete';
+    newRow.appendChild(deleteBook);
+    deleteBook.addEventListener('click', () => {
+        
+    })
+
 }
 
-addBookBtn.addEventListener ('click', () => {
-    addBook();
+openModal.addEventListener('click', () => dialog.showModal());
+
+submit.addEventListener('click', () => {
+    addBookModal();
     displayBook(library);
 
     console.table(library);
 })
+
+// Add a button on each book’s display to remove the book from the library.
+// You will need to associate your DOM elements with the actual book objects in some way. 
+// One easy solution is giving them a data-attribute that corresponds to the index of the library array.
+
+// function addBook() {
+//     const addTitle = prompt('What is the title of the book?');
+//     const addAuthor = prompt('Who is the author of the book?');
+//     const addPages = prompt('How many pages does the book have?');
+//     const readStatus = prompt('Have you read the book?');
+
+//     return library.push(new Book(addTitle, addAuthor, addPages, readStatus));
+// }
+
+
+
+// addBookBtn.addEventListener ('click', () => {
+//     addBook();
+//     displayBook(library);
+
+//     console.table(library);
+// })
