366f2e9a311bf8cfcbb7d33da74ef3c3aade01e8
Hossein Abbaszadegan <haabaszadegan@gmail.com>
2024-03-04 01:08:47 +0330

    Added toggle button for read column, next is...
    Need to implement interactivity for toggle button.

--- script.js
+++ script.js
@@ -24,7 +24,12 @@ function addBookModal() {
     const addTitle = title.value;
     const addAuthor = author.value;
     const addPages = pages.value;
-    const readStatus = read.value;
+    let readStatus;
+    if (read.checked) {
+        readStatus = 'Yes';
+    } else {
+        readStatus = 'No';
+    }
 
     return library.push(new Book(addTitle, addAuthor, addPages, readStatus));
 }
@@ -35,7 +40,6 @@ function displayBook(lib) {
     let newRow = '';
 
     for (const row of rows) {
-        // Assuming the title is stored in the first cell (td) of each row
         const titleCell = row.querySelector('td:first-child');
         bookTitles.push(titleCell.textContent);
     }
@@ -44,21 +48,37 @@ function displayBook(lib) {
         if (bookTitles.includes(book.title)) {
             continue;
         }
-         newRow = document.createElement('tr');
+        newRow = document.createElement('tr');
+        newRow.setAttribute('data-index', lib.length - 1);
         for (const property in book) {
             const newCell = document.createElement('td');
             newCell.textContent = book[property];
             newRow.appendChild(newCell);
+            createReadButton (book, property, newCell);
         }
         table.appendChild(newRow);
     }
+    createDeleteButton(newRow, lib);
+}
+
+function createReadButton (bo, prop, cell) {
+    if(prop === 'read') {  
+        let readBtn = document.createElement('button');
+        readBtn.textContent = bo[prop];
+        cell.textContent = '';
+        cell.appendChild(readBtn);
+    }
+}
+
+function createDeleteButton (row, libArray) {
     const deleteBook = document.createElement('button');
     deleteBook.textContent = 'Delete';
-    newRow.appendChild(deleteBook);
+    row.appendChild(deleteBook);
     deleteBook.addEventListener('click', () => {
-        
+        libArray.splice(libArray.length - 1, 1);
+        console.log(libArray);
+        row.remove();
     })
-
 }
 
 openModal.addEventListener('click', () => dialog.showModal());
@@ -69,25 +89,3 @@ submit.addEventListener('click', () => {
 
     console.table(library);
 })
-
-// Add a button on each book’s display to remove the book from the library.
-// You will need to associate your DOM elements with the actual book objects in some way. 
-// One easy solution is giving them a data-attribute that corresponds to the index of the library array.
-
-// function addBook() {
-//     const addTitle = prompt('What is the title of the book?');
-//     const addAuthor = prompt('Who is the author of the book?');
-//     const addPages = prompt('How many pages does the book have?');
-//     const readStatus = prompt('Have you read the book?');
-
-//     return library.push(new Book(addTitle, addAuthor, addPages, readStatus));
-// }
-
-
-
-// addBookBtn.addEventListener ('click', () => {
-//     addBook();
-//     displayBook(library);
-
-//     console.table(library);
-// })
